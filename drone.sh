9aa4ff0d905bb1366f09f756790c558594b3c781
Christopher Goddard <chrisjgoddard@gmail.com>
2019-11-01 18:44:50 +1100

    Configure basic config for drone server

---
+++ drone.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# to run
+# ssh root@_secret_ip_
+# curl -O -L https://raw.githubusercontent.com/token-cjg/hello_drone/master/drone.sh
+# ./drone.sh 
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+# sudo systemctl status docker
+
+# docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# nginx
+sudo apt-get install nginx
+sudo ufw allow 'Nginx Full'
+sudo ufw status
+sudo systemctl status nginx
+
+# nginx, HTTPS /w lets encrypt
+# note, need a domain - get one from freenom
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sudo apt-get install python-certbot-nginx -y
+sed -i 's/server_name _/server_name groklemins.tk drone.groklemins.tk/' /etc/nginx/sites-available/default
+sudo nginx -t
+sudo systemctl reload nginx
+sudo certbot --nginx -d groklemins.tk -d drone.groklemins.tk --no-redirect --register-unsafely-without-email --agree-tos
+sudo certbot renew --dry-run
