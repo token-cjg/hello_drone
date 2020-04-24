0c932ebbdc7602787a9273a1a4a881821203d55d
Christopher Goddard <chrisjgoddard@gmail.com>
2020-04-25 06:59:58 +1000

    Further simplification. use drone.groklemins.tk

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ DRONE_SECRET=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 65 && echo)
 sudo touch /etc/drone/server.env
 sudo bash -c "cat << EOF > /etc/drone/server.env
 # Service settings
-DRONE_SERVER_HOST=groklemins.tk
+DRONE_SERVER_HOST=drone.groklemins.tk
 DRONE_SERVER_PROTO=https
 DRONE_TLS_AUTOCERT=false
 DRONE_RUNNER_CAPACITY=3
@@ -36,21 +36,4 @@ sudo mv drone.service /etc/systemd/system/drone.service
 
 sudo mv /etc/nginx/sites-enabled/default /etc/nginx/sites-available
 curl -O -L https://raw.githubusercontent.com/token-cjg/hello_drone/master/fixtures/sites_enabled.default
-sudo mv sites_enabled.default /etc/nginx/sites-enabled
-sudo mv /etc/nginx/sites-enabled/sites_enabled.default /etc/nginx/sites-enabled/default
-
-#sudo sed -i 's/server_name _/server_name groklemins.tk drone.groklemins.tk/' /etc/nginx/sites-enabled/default
-#sudo sed -i 's/try_files $uri $uri\/ =404;/proxy_pass http:\/\/drone;/' /etc/nginx/sites-enabled/default
-#sudo bash -c "cat << EOT >> /etc/nginx/sites-enabled/default
-# upstream drone {
-#     server 127.0.0.1:8000;
-# }
-#
-# map \$http_upgrade \$connection_upgrade {
-#     default upgrade;
-#     ''      close;
-# }
-# EOT"
-
-# curl -O -L https://raw.githubusercontent.com/token-cjg/hello_drone/master/fixtures/sites_enabled.default
-# sudo mv sites_enabled.default /etc/nginx/sites-enabled/default
+sudo mv sites_enabled.default /etc/nginx/sites-enabled/default
